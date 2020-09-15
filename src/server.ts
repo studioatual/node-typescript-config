d0b1c390ce5807e521145d544f402bb2000b0151
Willian Souza <contato@studioatual.com>
2020-09-15 06:41:24 -0300

    chore: add jest

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,10 @@
-import express from 'express';
-import '@controllers/UserController';
+import express from 'express'
+import '@controllers/UserController'
 
-const app = express();
+const app = express()
 
 app.get('/', (req, res) => {
-  return res.json({ message: 'Hello World!' });
-});
+  return res.json({ message: 'Hello World!' })
+})
 
-app.listen(3333);
+app.listen(3333)
